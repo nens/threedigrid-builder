10725a473d6a00807532cb2cf03828fd31a2388b
Martijn Siemerink <martijn.siemerink@nelen-schuurmans.nl>
2019-01-15 16:46:09 +0100

    WIP

--- setup.py
+++ setup.py
@@ -13,14 +13,16 @@ if platform.system() == 'Linux':
     f_macros = [('F2PY_REPORT_ON_ARRAY_COPY', '1'), 
 				('NPY_NO_DEPRECATED_API', '1')]
     f_include = ['/usr/include', '/usr/include/hdf5/serial', '/opt/threedicore/include']
-    f_lib = ['/usr/lib/x86_64-linux-gnu/, /usr/lib/x86_64-linux-gnu/hdf5/serial','/opt/threedicore/lib']
+    f_lib = ['/opt/threedicore/lib']
     f_libs = ['flow']
 else:
     f_args = ["/Od", "/debug:full", "/assume:nounderscore"]
-    f_macros = [('NO_APPEND_FORTRAN','1')]
-    f_include = ['../core_flow1d2d/x64/Debug', '../netcdf']
-    f_lib = ['../core_flow1d2d/x64/Debug', '../netcdf']
-    f_libs = ['libthreedicore']
+    f_macros = [('NO_APPEND_FORTRAN','1'),
+                ('F2PY_REPORT_ON_ARRAY_COPY', '1'), 
+				('NPY_NO_DEPRECATED_API', '1')]
+    f_include = ['../threedi-calculationcore/core_flow1d2d/x64/Debug', '../threedi-calculationcore/lgpl/x64/Debug']
+    f_lib = ['../threedi-calculationcore/core_flow1d2d/x64/Debug', '../threedi-calculationcore/lgpl/x64/Debug']
+    f_libs = ['libflow']
 
 api_module = Extension(
     name= 'py3di',
