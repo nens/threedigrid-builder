2b3413529131915ad29db6d328112d1926e97e9c
Martijn Siemerink <martijn.siemerink@nelen-schuurmans.nl>
2021-02-24 11:29:23 +0100

    add doc stirngs to quadtree.py.

--- threedigrid_builder/grid2d/quadtree.py
+++ threedigrid_builder/grid2d/quadtree.py
@@ -7,7 +7,8 @@ __all__ = ["QuadTree"]
 
 
 class QuadTree:
-
+    """Defines active cell levels for computational grid.
+    """
     def __init__(
         self,
         origin,
@@ -19,11 +20,11 @@ class QuadTree:
         refinements
     ):
 
-        self.lgrmin = min_gridsize / pixel_size
-        self.kmax = num_refine_levels
-        self.mmax = np.empty((self.kmax,), dtype=np.int32, order='F')
-        self.nmax = np.empty((self.kmax,), dtype=np.int32, order='F')
-        self.dx = np.empty((self.kmax,), dtype=np.float64, order='F')
+        self._lgrmin = min_gridsize / pixel_size
+        self._kmax = num_refine_levels
+        self._mmax = np.empty((self.kmax,), dtype=np.int32, order='F')
+        self._nmax = np.empty((self.kmax,), dtype=np.int32, order='F')
+        self._dx = np.empty((self.kmax,), dtype=np.float64, order='F')
 
         max_grid_x_pix = self._determine_max_quadtree_pixels(
             subgrid_width
@@ -31,14 +32,21 @@ class QuadTree:
         max_grid_y_pix = self._determine_max_quadtree_pixels(
             subgrid_height
         )
-        self.mmax[:] = max_grid_x_pix / (self.lgrmin * 2 ** np.arange(0, self.kmax))
-        self.nmax[:] = max_grid_y_pix / (self.lgrmin * 2 ** np.arange(0, self.kmax))
+        self.mmax[:] = max_grid_x_pix / (
+            self.lgrmin * 2 ** np.arange(0, self.kmax)
+        )
+        self.nmax[:] = max_grid_y_pix / (
+            self.lgrmin * 2 ** np.arange(0, self.kmax)
+        )
         self.dx[:] = self.lgrmin * 2 ** np.arange(0, self.kmax) * pixel_size
         ur_corner = (
             origin[0] + max_grid_x_pix * pixel_size,
             origin[1] + max_grid_y_pix * pixel_size
         )
         self.bbox = np.array(tuple(self.origin) + ur_corner)
+
+        # Array with dimensions of smallest active grid level and contains
+        # map of active grid level for each quadtree cell.
         self.lg = np.full(
             (self.mmax[0], self.nmax[0]),
             self.kmax,
@@ -49,12 +57,57 @@ class QuadTree:
         self.set_refinements(refinements)
         self.create_quadtree()
 
+    @property
+    def min_cell_pixels(self):
+        """Returns minimum number of pixels in smallles computational_cell.
+        """
+        return self._lgrmin
+
+    @property
+    def kmax(self):
+        """Returns maximum number of active grid levels in quadtree.
+        """
+        return self._kmax
+
+    @property
+    def mmax(self):
+        """Returns array with column dimensions of every
+        active grid level [0:kmax].
+        """
+        return self._mmax
+
+    @property
+    def nmax(self):
+        """Returns array with row dimensions of every
+        active grid level [0:kmax].
+        """
+        return self._mmax
+
     def _determine_max_quadtree_pixels(self, side):
+        """Compute pixels dimension of active quadtree. (extend raster
+        dimension to quadtree cell bound.)
+
+        Args:
+          number of pixels of raster dimension.
+
+        Returns:
+          Value of pixel dimension of quadtree dimension.
+        """
         max_pix_largest_cell = self.lgrmin * 2 ** (self.kmax - 1)
         return (max_pix_largest_cell * (side / (max_pix_largest_cell))) \
             + max_pix_largest_cell
 
     def set_refinements(self, refinements):
+        """Set active grid levels for based on refinement dict and 
+        filling lg variable for refinement locations.
+
+        Args:
+          dict of refinemnets containing at least
+          - geometry
+          - refinement_level
+          - id
+
+        """
         for i in range(len(refinements['id'])):
             geom = np.asfortranarray(
                 pygeos.get_coordinates(refinements['the_geom'][i])
@@ -73,4 +126,7 @@ class QuadTree:
             )
 
     def create_quadtree(self):
+        """Actually creating quadtree with correct level transitions for 
+        quadtree cells.
+        """
         return create_quadtree(self.kmax, self.mmax, self.nmax, self.lg)
