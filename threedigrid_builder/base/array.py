ccc61a6bd34e7544229863e51d80d319b6837031
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-02-24 09:32:00 +0100

    Annotations

---
+++ threedigrid_builder/base/array.py
@@ -0,0 +1,137 @@
+from enum import Enum
+from typing import Tuple
+import numpy as np
+from functools import update_wrapper
+
+
+def _to_ndarray(value, elem_type, expected_length):
+    """Cast value to numpy array, with some type mappings.
+    
+    Args:
+        value (iterable or None): iterable to convert to ndarray
+        elem_type (type)
+        expected_length (int or None): the expected number of rows
+    """
+    if value is None:
+        return
+    # Tuple[...] becomes 2D Array
+    if issubclass(elem_type, Tuple):
+        sub_types = elem_type.__args__
+        n_columns = len(sub_types)
+        elem_type = sub_types[0]
+    else:
+        n_columns = None
+
+    # cast python to numpy dtypes
+    if elem_type is int or issubclass(elem_type, Enum):
+        dtype = np.int32
+    elif elem_type is float:
+        dtype = np.float64
+    elif elem_type is bool:
+        dtype = bool
+    else:
+        dtype = object
+
+    arr = np.asarray(value, dtype=dtype, order="F")
+    if n_columns is None and arr.ndim != 1:
+        raise ValueError(
+            f"Expected a one dimensional array, got {arr.ndim}."
+        )
+    elif n_columns is not None and arr.ndim != 2:
+        raise ValueError(
+            f"Expected a two dimensional array, got {arr.ndim}."
+        )
+    elif arr.ndim == 2 and arr.shape[1] != n_columns:
+        raise ValueError(f"Expected {n_columns} columns, got {arr.shape[1]}.")
+
+    if expected_length is not None and arr.shape[0] != expected_length:
+        raise ValueError(f"Expected {expected_length} rows, got {arr.shape[0]}.")
+
+    return arr
+
+
+class array_of:
+    """A decorator to create an array dataclass from a normal dataclass.
+    
+    The decorated class will have the same attributes as the provided dataclass
+    but then transformed into 1D numpy arrays.
+
+    Supported types are:
+    - float (cast to numpy.float64)
+    - int (cast to numpy.int32)
+    - bool (cast to numpy._bool)
+    - Enum (cast to numpy.int32)
+    - Tuple[...] (cast to numpy.float64)
+
+    Other types are not converted to numpy dtypes and kept as python object.
+
+    The id field is required.
+
+    Example:
+    >>> class MyRecord:
+    ...     id: int
+    ...
+    >>> @array_of(MyRecord)
+    >>> class MyRecords:
+    >>>     pass
+    ...
+    >>> MyRecords(id=[1, 2, 3])
+    < array of MyRecord (len:3) >
+    """
+
+    def __init__(self, data_class):
+        """Validate the dataclass passed into the decorator"""
+
+        # check the subtypes of Tuples
+        fields = data_class.__annotations__
+        for name, elem_type in fields.items():
+            if issubclass(elem_type, Tuple):
+                sub_types = elem_type.__args__
+                n_columns = len(sub_types)
+                if n_columns == 0:
+                    raise TypeError("Tuple cannot contain 0 elements")
+                elem_type = sub_types[0]
+                if not all(x is elem_type for x in sub_types):
+                    raise TypeError("Tuple cannot contain mixed dtypes")
+
+        self.data_class = data_class
+
+    def __call__(self, cls):
+        """Wrap a class by subclassing it.
+
+        Note that the below example is equal to the normal decorator usage
+        illustrated above.
+    
+        >>> class MyRecords:
+        ...     pass
+        ...
+        >>> MyRecords = array_of(MyRecord)(MyRecords)
+        """
+        class Wrapper(cls):
+            def __init__(self, **kwargs):
+                """Initialize the array dataclass
+                
+                Take the iterables in the arguments and convert them into 1D
+                ndarrays according to the types in the normal dataclass.
+                """
+                # id is required; get its length to use it for validation
+                expected_length = len(kwargs["id"])
+
+                # convert each field to an array and set it to self
+                fields = data_class.__annotations__
+                for name, elem_type in fields.items():
+                    value = kwargs.get(name, None)
+                    try:
+                        arr = _to_ndarray(value, elem_type, expected_length)
+                    except ValueError as e:
+                        raise ValueError("Error parsing {name} to array: {e}")
+                    setattr(self, name, arr)
+
+                # call the init of the wrapped class
+                super().__init__(**kwargs)
+
+            def __repr__(self):
+                return f"<array of {self._type.__name__} (len:{len(self.id)})>"
+
+        # update_wrapper transfers __name__, __doc__, etc. from cls to Wrapper
+        return update_wrapper(Wrapper, cls)
---
+++ threedigrid_builder/base/constants.py
@@ -0,0 +1,51 @@
+from enum import Enum, unique
+
+
+@unique
+class CalculationType(Enum):
+    # -1 tot 99 ::= Node | 'verbinding-zonder-subnodes(="vertices")'
+    BOUNDARY_NODE = -1
+    EMBEDDED_NODE = 0
+    STANDALONE_NODE = 1
+    CONNECTED_NODE = 2
+    BROAD_CRESTED = 3  # only orifices + weirs, corresponds
+    SHORT_CRESTED = 4  # only orifices + weirs
+    DOUBLE_CONNECTED = 5
+    # 100 tot oneindig ::= 'verbinding-met-subnodes(="vertices")'
+    # a.k.a. pinpoint
+    EMBEDDED_VERTEX = 100
+    STANDALONE_VERTEX = 101
+    CONNECTED_VERTEX = 102
+    DOUBLE_CONNECTED_VERTEX = 105
+
+
+@unique
+class NodeType(Enum):
+    NODE_2D_OPEN_WATER = 1
+    NODE_2D_GROUNDWATER = 2
+    NODE_1D_NO_STORAGE = 3
+    NODE_1D_STORAGE = 4
+    NODE_2D_BOUNDARIES = 5
+    NODE_2D_GROUNDWATER_BOUNDARIES = 6
+    NODE_1D_BOUNDARIES = 7
+
+
+@unique
+class ContentType(Enum):
+    TYPE_V2_PIPE = 1
+    TYPE_V2_CHANNEL = 2
+    TYPE_V2_CULVERT = 3
+    TYPE_V2_ORIFICE = 4
+    TYPE_V2_WEIR = 5
+    TYPE_V2_MANHOLE = 6
+    TYPE_V2_PUMPSTATION = 7
+    TYPE_V2_LEVEE = 8
+    TYPE_V2_1D_LATERAL = 9
+    TYPE_V2_1D_BOUNDARY_CONDITIONS = 10
+    TYPE_V2_2D_BOUNDARY_CONDITIONS = 11
+    TYPE_V2_CONNECTION_NODES = 12
+    TYPE_V2_BREACH = 13
+    TYPE_V2_CROSS_SECTION_DEF = 14
+    TYPE_V2_CROSS_SECTION_LOCATION = 15
+    TYPE_V2_ADDED_CALCULATION_POINT = 16
+    TYPE_V2_WINDSHIELD = 17
