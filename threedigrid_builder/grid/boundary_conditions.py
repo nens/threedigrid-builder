576666aa69a1ac8a4688372698a1e8d657955573
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-09-08 15:56:28 +0200

    WIP

--- threedigrid_builder/grid/boundary_conditions.py
+++ threedigrid_builder/grid/boundary_conditions.py
@@ -8,6 +8,7 @@ from threedigrid_builder.constants import LineType
 from threedigrid_builder.constants import NodeType
 from threedigrid_builder.exceptions import SchematisationError
 
+import itertools
 import numpy as np
 import pygeos
 
@@ -99,61 +100,101 @@ class BoundaryCondition2D:
 class BoundaryConditions2D:
     def get_nodes(self, grid, node_id_counter):
         min_length = grid.quadtree.dx[0] / 2
-        max_aspect_ratio = 0.0875  # 5 degree slope
 
         nodes = Nodes(id=[])
         for bc_idx in range(len(self)):
             x1, y1, x2, y2 = pygeos.bounds(self.the_geom[bc_idx])
             is_horizontal = (x2 - x1) > (y2 - y1)
-            if is_horizontal:
-                length = x2 - x1
-                width = y2 - y1
-            else:
-                length = y2 - y1
-                width = x2 - x1
+            length = x2 - x1 if is_horizontal else y2 - y1
             if length < min_length:
                 raise SchematisationError(
                     f"Boundary condition {self.id[bc_idx]} is too small."
                 )
-            if width / length > max_aspect_ratio:
-                raise SchematisationError(
-                    f"Boundary condition {self.id[bc_idx]} is has incorrect angle."
-                )
+
             node_idx = np.sort(grid.cell_tree.query(self.the_geom[bc_idx]))
             nodk = grid.nodes.nodk[node_idx]
             sz = 2 ** (nodk - 1)
             nodm = grid.nodes.nodm[node_idx] - 1  # nodm indexes 1-based into quad_idx
             nodn = grid.nodes.nodn[node_idx] - 1  # nodn indexes 1-based into quad_idx
-            quad_idx = grid.quadtree.quad_idx - 1  # indexes in quad_idx are 1-based
-            assert np.all(quad_idx[nodm * sz, nodn * sz] == node_idx)
+            quad_idx = grid.quadtree.quad_idx  # indexes in quad_idx are 1-based
+            assert np.all(quad_idx[nodm * sz, nodn * sz] == (node_idx + 1))
 
             if is_horizontal:
                 b = nodn > 0
-                b[b] = quad_idx[nodm[b] * sz[b], nodn[b] * sz[b] - 1] != -1
-                b[b] = quad_idx[(nodm[b] + 1) * sz[b] - 1, nodn[b] * sz[b] - 1] != -1
+                b[b] = quad_idx[nodm[b] * sz[b], nodn[b] * sz[b] - 1] != 0
+                b[b] = quad_idx[(nodm[b] + 1) * sz[b] - 1, nodn[b] * sz[b] - 1] != 0
                 a = nodn + sz < quad_idx.shape[1]
-                a[a] = quad_idx[nodm[a] * sz[a], (nodn[a] + 1) * sz[a]] != -1
-                a[a] = quad_idx[(nodm[a] + 1) * sz[a] - 1, (nodn[a] + 1) * sz[a]] != -1
+                a[a] = quad_idx[nodm[a] * sz[a], (nodn[a] + 1) * sz[a]] != 0
+                a[a] = quad_idx[(nodm[a] + 1) * sz[a] - 1, (nodn[a] + 1) * sz[a]] != 0
             else:
                 b = nodm > 0
-                b[b] = quad_idx[nodm[b] * sz[b] - 1, nodn[b] * sz[b]] != -1
-                b[b] = quad_idx[nodm[b] * sz[b] - 1, (nodn[b] + 1) * sz[b] - 1] != -1
+                b[b] = quad_idx[nodm[b] * sz[b] - 1, nodn[b] * sz[b]] != 0
+                b[b] = quad_idx[nodm[b] * sz[b] - 1, (nodn[b] + 1) * sz[b] - 1] != 0
                 a = (nodm + 1) * sz < quad_idx.shape[0]
-                a[a] = quad_idx[(nodm[a] + 1) * sz[a], nodn[a] * sz[a]] != -1
-                a[a] = quad_idx[(nodm[a] + 1) * sz[a], (nodn[a] + 1) * sz[a] - 1] != -1
+                a[a] = quad_idx[(nodm[a] + 1) * sz[a], nodn[a] * sz[a]] != 0
+                a[a] = quad_idx[(nodm[a] + 1) * sz[a], (nodn[a] + 1) * sz[a] - 1] != 0
 
             # the edge of the boundary is whichever side has the most edges
             if np.count_nonzero(a) > np.count_nonzero(b):
-                is_edge = a
+                node_idx = node_idx[a]
                 kcu = (
                     LineType.LINE_2D_BOUNDARY_SOUTH
                     if is_horizontal
                     else LineType.LINE_2D_BOUNDARY_WEST
                 )
+                is_before = False
             else:
-                is_edge = b
+                node_idx = node_idx[b]
                 kcu = (
                     LineType.LINE_2D_BOUNDARY_NORTH
                     if is_horizontal
                     else LineType.LINE_2D_BOUNDARY_EAST
                 )
+                is_before = True
+
+            # check the edge location (coordinate should be the same for all cells)
+            bounds = grid.nodes.bounds[node_idx]
+            if kcu == LineType.LINE_2D_BOUNDARY_WEST:
+                edge_coord = bounds[:, 0]
+            elif kcu == LineType.LINE_2D_BOUNDARY_EAST:
+                edge_coord = bounds[:, 2]
+            elif kcu == LineType.LINE_2D_BOUNDARY_NORTH:
+                edge_coord = bounds[:, 3]
+            elif kcu == LineType.LINE_2D_BOUNDARY_SOUTH:
+                edge_coord = bounds[:, 1]
+
+            if edge_coord.size > 1 and np.any(edge_coord[1:] != edge_coord[:-1]):
+                coords = np.unique(edge_coord)
+                axis = "y" if is_horizontal else "x"
+                raise SchematisationError(
+                    f"2D boundary condition {self.id[bc_idx]} touches cells with "
+                    f"different edge coordinates ({axis}={coords.tolist()})."
+                )
+
+            # now create the new nodes
+            new_nodes = grid.nodes[node_idx]
+            new_nodes.id[:] = list(itertools.islice(node_id_counter, len(new_nodes)))
+            new_nodes.boundary_id[:] = self.id[bc_idx]
+            new_nodes.boundary_type[:] = self.boundary_type[bc_idx]
+            new_nodes.node_type[:] = NodeType.NODE_2D_BOUNDARIES
+            new_nodes.nodk[:] = -9999
+            new_nodes.nodm[:] = -9999
+            new_nodes.nodn[:] = -9999
+            size = new_nodes.bounds[:, 2] - new_nodes.bounds[:, 0]
+            size_px = new_nodes.pixel_coords[:, 0] - new_nodes.pixel_coords[:, 2]
+            if is_horizontal:
+                axes = (1, 3)
+            else:
+                axes = (0, 2)
+            if is_before:
+                size *= -1
+                size_px *= -1
+            new_nodes.bounds[:, axes[0]] -= size
+            new_nodes.bounds[:, axes[1]] -= size
+            new_nodes.coordinates[:, axes[0]] -= size
+            new_nodes.pixel_coords[:, axes[0]] -= size_px
+            new_nodes.pixel_coords[:, axes[1]] -= size_px
+
+            nodes += new_nodes
+
+        return nodes
--- threedigrid_builder/grid/grid.py
+++ threedigrid_builder/grid/grid.py
@@ -465,7 +465,7 @@ class Grid:
         boundary_conditions_1d.apply(self)
 
     def set_boundary_conditions_2d(self, boundary_conditions_2d, node_id_counter):
-        nodes = boundary_conditions_2d.get_nodes(self, node_id_counter)
+        self.nodes += boundary_conditions_2d.get_nodes(self, node_id_counter)
 
     def set_pumps(self, pumps):
         """Set the pumps on this grid object
