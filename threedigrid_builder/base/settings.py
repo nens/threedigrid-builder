d6aaa17621bf3972ba3854d3635c4bd3940386be
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-11-17 10:24:43 +0100

    [Done] Validate settings (#149)

--- threedigrid_builder/base/settings.py
+++ threedigrid_builder/base/settings.py
@@ -1,10 +1,24 @@
 from dataclasses import dataclass
 from dataclasses import fields
+from threedigrid_builder.base import is_int_enum
+from threedigrid_builder.base import unpack_optional_type
+from threedigrid_builder.constants import FrictionType
+from threedigrid_builder.constants import InfiltrationSurfaceOption
 from threedigrid_builder.constants import InitializationType
+from threedigrid_builder.constants import InterflowType
+from threedigrid_builder.exceptions import SchematisationError
 from typing import Optional
+
+import numpy as np
 
 
 __all__ = ["GridSettings", "TablesSettings"]
+
+
+def greater_zero_check(obj, attr):
+    value = getattr(obj, attr, None)
+    if value is not None and value <= 0:
+        raise SchematisationError(f"'{attr}' must be greater than 0.")
 
 
 @dataclass
@@ -19,7 +33,7 @@ class GridSettings:
     dist_calc_points: float
     kmax: int
     embedded_cutoff_threshold: float = 0.05
-    max_angle_1d_advection: float = 90.0
+    max_angle_1d_advection: float = 0.4 * np.pi
 
     @classmethod
     def from_dict(cls, dct):
@@ -28,6 +42,11 @@ class GridSettings:
         return cls(
             **{k: v for k, v in dct.items() if k in class_fields and v is not None}
         )
+
+    def __post_init__(self):
+        # validations
+        for field in ("grid_space", "dist_calc_points", "kmax"):
+            greater_zero_check(self, field)
 
 
 @dataclass
@@ -38,7 +57,7 @@ class TablesSettings:
     table_step_size: float
     frict_coef: float
     frict_coef_type: InitializationType
-    frict_type: int = 2
+    frict_type: FrictionType = FrictionType.MANNING
     interception_global: Optional[float] = None
     interception_type: Optional[InitializationType] = None
     table_step_size_1d: float = None  # actual default is set in __post_init__
@@ -66,7 +85,7 @@ class TablesSettings:
     groundwater_hydro_connectivity_type: Optional[InitializationType] = None
 
     ## from Interflow
-    interflow_type: int = 0  # InterflowType (0 means: no interflow)
+    interflow_type: InterflowType = InterflowType.NO_INTERLFOW
     porosity: Optional[float] = None
     porosity_type: Optional[InitializationType] = None
     porosity_layer_thickness: Optional[float] = None
@@ -77,15 +96,37 @@ class TablesSettings:
     ## from SimpleInfiltration
     infiltration_rate: Optional[float] = None
     infiltration_rate_type: Optional[InitializationType] = None
-    infiltration_surface_option: Optional[int] = None  # InfiltrationSurfaceOption
+    infiltration_surface_option: InfiltrationSurfaceOption = (
+        InfiltrationSurfaceOption.RAIN
+    )
     max_infiltration_capacity_type: Optional[InitializationType] = None
 
     def __post_init__(self):
-        # set some defaults
+        # defaults
         if self.table_step_size_1d is None:
             self.table_step_size_1d = self.table_step_size
         if self.table_step_size_volume_2d is None:
             self.table_step_size_volume_2d = self.table_step_size
+
+        # validations
+        for field in (
+            "table_step_size",
+            "table_step_size_1d",
+            "table_step_size_volume_2d",
+        ):
+            greater_zero_check(self, field)
+
+        # check enums
+        for (name, elem_type) in self.__annotations__.items():
+            elem_type = unpack_optional_type(elem_type)
+            if is_int_enum(elem_type):
+                value = getattr(self, name)
+                if value is None:
+                    continue
+                try:
+                    elem_type(value)
+                except ValueError as e:
+                    raise SchematisationError(str(e))
 
     @classmethod
     def from_dict(cls, dct):
