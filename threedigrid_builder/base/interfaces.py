82a70b6ec23e9801f172e651476d9a0117cabc52
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-11-25 09:28:48 +0100

    Refactor Subgrid into RasterioInterface

--- threedigrid_builder/base/interfaces.py
+++ threedigrid_builder/base/interfaces.py
@@ -4,9 +4,9 @@ from .pumps import Pumps
 from abc import ABC
 from abc import abstractmethod
 from pathlib import Path
+from typing import Optional
 
-
-__all__ = ["OutputInterface"]
+__all__ = ["OutputInterface", "RasterInterface"]
 
 
 class OutputInterface(ABC):
@@ -42,4 +42,28 @@ class OutputInterface(ABC):
 
     @abstractmethod
     def write_nodes_embedded(self, nodes_embedded: Nodes):
+        pass
+
+
+class RasterInterface(ABC):
+    """The metaclass (class template) for raster data input"""
+
+    def __init__(self, path: Path, model_area_path: Optional[Path] = None):
+        self.path = path
+        self.model_area_path = model_area_path
+        super().__init__()
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} of file {self.path}>"
+
+    @abstractmethod
+    def __enter__(self):
+        pass
+
+    @abstractmethod
+    def __exit__(self, type, value, traceback):
+        pass
+
+    @abstractmethod
+    def get_meta(self):
         pass
--- threedigrid_builder/interface/subgrid.py
+++ threedigrid_builder/interface/subgrid.py
@@ -1,5 +1,7 @@
 import json
 import numpy as np
+from pathlib import Path
+from threedigrid_builder.base import RasterInterface
 
 
 try:
@@ -10,73 +12,62 @@ except ImportError:
     rasterio = rasterio_features = None
 
 
-class Subgrid:
-    def __init__(self, filepath, model_area=None):
+class RasterioInterface(RasterInterface):
+    def __enter__(self):
+        self._env = rasterio.Env().__enter__()
+        self._raster = rasterio.open(self.path, "r").__enter__()
+        self.pixel_size = self._raster.profile["transform"][0]
 
-        self.filepath = filepath
-        with rasterio.Env():
-            with rasterio.open(filepath, "r") as dem:
-                self.transform = dem.profile["transform"]
-                self._dxp = self.transform[0]
-                if model_area is not None:
-                    area_geometry = self._load_geometry(model_area)
-                    self._area_raster = self._create_area_arr_from_geometry(
-                        dem, area_geometry
-                    )
-                else:
-                    self._area_raster = self._create_area_arr_from_dem(dem)
+    def __exit__(self, *args, **kwargs):
+        self._raster.__exit__(*args, **kwargs)
+        self._env.__exit__(*args, **kwargs)
 
-    def __repr__(self):
-        return f"<SubgridMeta object of raster file {self.filepath}>"
+    def read(self):
+        if self.model_area_path is not None:
+            area_geometry = self._load_geometry(self.model_area_path)
+            width, height, bbox, data = self._create_area_arr_from_geometry(area_geometry)
+        else:
+            width, height, bbox, data = self._create_area_arr_from_dem()
 
-    @property
-    def mask(self):
-        return np.flipud(self._area_raster).T.astype(
+        return {
+            "pixel_size": self.pixel_size,
+            "width": width,
+            "height": height,
+            "bbox": bbox,
+            "area_mask": np.flipud(data).T.astype(
             dtype=np.int32, copy=False, order="F"
-        )
+                ),
+        }
 
-    @property
-    def bbox(self):
-        return self._bbox
+    def _create_area_arr_from_dem(self):
+        data = self._raster.read_masks(1)
+        data[data > 0] = 1
+        return self._raster.width, self._raster.height, self._raster.bounds, data
 
-    @property
-    def pixel_size(self):
-        return self._dxp
-
-    @property
-    def width(self):
-        return self._width
-
-    @property
-    def height(self):
-        return self._height
-
-    @property
-    def offset_model_area_dem(self):
-        return (self._i_off, self._j_off)
-
-    def _load_geometry(self, model_area_json):
+    @staticmethod
+    def _load_geometry(model_area_json):
         with open(model_area_json, "r") as f:
             area_geometry = json.load(f)["features"][0]
         return area_geometry
 
-    def _create_area_arr_from_geometry(self, dem, area_geometry):
+
+    def _create_area_arr_from_geometry(self, area_geometry):
+        dem = self._raster
 
         window = rasterio_features.geometry_window(
             dem, (area_geometry,), pixel_precision=3
         )
-        self._height = window.height
-        self._width = window.width
         left = dem.bounds.left + window.col_off * self.pixel_size
         top = dem.bounds.top + window.row_off * -self.pixel_size
-        self._bbox = (
+        bbox = (
             left,
             top + window.height * -self.pixel_size,
             left + window.width * self.pixel_size,
             top,
         )
-        self._i_off = window.col_off
-        self._j_off = dem.height - window.row_off - window.height
+        # self._i_off = window.col_off
+        # self._j_off = dem.height - window.row_off - window.height
+        # offset_model_area_dem = (self._i_off, self._j_off)
         self.transform = rasterio.transform.from_origin(
             left, top, self.pixel_size, self.pixel_size
         )
@@ -87,40 +78,22 @@ class Subgrid:
             transform=self.transform,
             dtype=np.int32,
         )
-        return data
+        return window.width,window.height,bbox,data
 
-    def _create_area_arr_from_dem(self, dem):
-        self._bbox = dem.bounds
-        self._height = dem.height
-        self._width = dem.width
-        self._i_off = self._j_off = 0
-        data = dem.read_masks(1)
-        data[data > 0] = 1
+    
 
-        return data
+    # def create_model_area_tiff(self, out_filepath):
 
-    def get_meta(self):
-        """Return meta information of subgrid from raster."""
-        return {
-            "pixel_size": self.pixel_size,
-            "width": self.width,
-            "height": self.height,
-            "bbox": self.bbox,
-            "area_mask": self.mask,
-        }
-
-    def create_model_area_tiff(self, out_filepath):
-
-        with rasterio.Env():
-            with rasterio.open(self.dem_filepath, "r") as dem:
-                profile = dem.profile
-                profile.update(
-                    width=self.width,
-                    height=self.height,
-                    out_shape=(self.height, self.width),
-                    transform=self.transform,
-                    dtype=np.int32,
-                    compress="lzw",
-                )
-                with rasterio.open(out_filepath, "w", **profile) as area_tiff:
-                    area_tiff.write(self._area_raster.astype(np.int32), 1)
+    #     with rasterio.Env():
+    #         with rasterio.open(self.dem_filepath, "r") as dem:
+    #             profile = dem.profile
+    #             profile.update(
+    #                 width=self.width,
+    #                 height=self.height,
+    #                 out_shape=(self.height, self.width),
+    #                 transform=self.transform,
+    #                 dtype=np.int32,
+    #                 compress="lzw",
+    #             )
+    #             with rasterio.open(out_filepath, "w", **profile) as area_tiff:
+    #                 area_tiff.write(self._area_raster.astype(np.int32), 1)
