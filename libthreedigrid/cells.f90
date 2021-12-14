566f7bac220ea4f9264f561d24ef274453ef6a99
Martijn Siemerink <martijn.siemerink@nelen-schuurmans.nl>
2021-12-14 15:31:11 +0100

    Add f2py files

---
+++ libthreedigrid/cells.f90
@@ -0,0 +1,221 @@
+module m_cells
+
+    use iso_fortran_env, only: int8
+
+    contains
+
+    subroutine set_2d_computational_nodes_lines(origin, lgrmin, kmax, mmax, nmax, dx, lg,&
+        quad_idx, area_mask, nodk, nodm, nodn, bounds, coords, pixel_coords,&
+        line,cross_pix_coords, n_line_u)
+        !!! Entry point for setting nodes and lines and there necessary attributes.
+        use m_grid_utils, only : get_lg_corners, get_cell_bbox, get_pix_corners, pad_area_mask
+        use iso_fortran_env, only : int8
+
+        double precision, intent(in) :: origin(2) ! Origin of Quadtree grid
+        integer, intent(in) :: lgrmin ! Number of pixels in cell of smallest refinement level
+        integer, intent(in) :: kmax ! Maximum refinement levels
+        integer, intent(in) :: mmax(:) ! X Dimension of each refinement level
+        integer, intent(in) :: nmax(:) ! Y Dimension of each refinement level
+        double precision, intent(in) :: dx(:) ! Cell size of each refinement level
+        integer, intent(inout) :: lg(:, :) ! Array with all refinement levels.
+        integer, intent(inout) :: quad_idx(:, :) ! Array with idx of cell at lg refinement locations
+        integer(kind=int8), intent(inout) :: area_mask(:, :) ! Array with active pixels of model.
+        integer, intent(inout) :: nodk(:) ! Array with refinement level of comp node
+        integer, intent(inout) :: nodm(:) ! Array with x or m coordinate on its refinement level grid
+        integer, intent(inout) :: nodn(:) ! Array with y or n coordinate on its refinement level grid
+        double precision, intent(inout) :: bounds(:, :) ! Bbox of comp cell
+        double precision, intent(inout) :: coords(:, :) ! Cell center coordinates
+        integer, intent(inout) :: pixel_coords(:, :) ! pixel bbox of comp cell
+        integer, intent(inout) :: line(:, :) ! Array with connecting nodes of line.
+        integer, intent(inout) :: cross_pix_coords(:, :) ! Array pixel indices of line interface
+        integer, intent(in) :: n_line_u ! Number of active u-dir lines.
+        integer(kind=int8), allocatable :: area_mask_padded(:, :)
+        integer :: nod
+        integer :: k
+        integer :: i0, i1, j0, j1
+        integer :: l_u, l_v
+        integer :: m, n
+        integer :: mn(4)
+
+        write(*,*) '** INFO: Start setting 2D calculation cells.'
+        nod = 1
+        l_u = 0
+        l_v = n_line_u
+        line = 0
+        call get_pix_corners(kmax, mmax(kmax), nmax(kmax), lgrmin, i0, i1, j0, j1)
+        area_mask_padded = pad_area_mask(area_mask, i0, i1, j0, j1) 
+        do k=kmax, 1, -1
+            do m=1,mmax(k)
+                do n=1,nmax(k)
+                    mn = get_lg_corners(k, m, n)
+                    if(all(quad_idx(mn(1):mn(3),mn(2):mn(4)) == nod)) then
+                        nodk(nod) = k
+                        nodm(nod) = m
+                        nodn(nod) = n
+                        bounds(nod,:) = get_cell_bbox(origin(1), origin(2), m, n, dx(k))
+                        coords(nod, :) = (/ 0.5d0 * (bounds(nod,1) + bounds(nod,3)), 0.5d0 * (bounds(nod,2) + bounds(nod,4)) /)
+                        call get_pix_corners(k, m, n, lgrmin, i0, i1, j0, j1)
+                        ! We inverse the y-axis for pixel_coords to comply with geotiffs in future use.
+                        ! And do some index fiddling because python starts indexing at 0 and has open end indexing.
+                        pixel_coords(nod, :) = (/ i0 - 1, j0 - 1, i1, j1 /)
+                        call set_2d_computational_lines(l_u, l_v, k, m, n, mn, lg, lgrmin,&
+                            area_mask_padded, quad_idx, nod, line, cross_pix_coords)
+                        nod = nod + 1
+                    else
+                        continue
+                    endif
+                enddo
+            enddo
+        enddo
+        deallocate(area_mask_padded)
+        write(*,*) '** INFO: Number of 2D nodes is: ', nod - 1
+        write(*,*) '** INFO: Number of 2D lines is: ', l_u + l_v
+        write(*,*) '** INFO: Done setting 2D calculation cells.'
+
+    end subroutine set_2d_computational_nodes_lines
+
+    subroutine set_2d_computational_lines(l_u, l_v, k, m, n, mn, lg, lgrmin, area_mask, quad_idx, nod, line, cross_pix_coords)
+
+        use m_grid_utils, only : get_lg_corners, get_pix_corners, crop_pix_coords_to_raster
+
+        integer, intent(inout) :: l_u
+        integer, intent(inout) :: l_v
+        integer, intent(in) :: k
+        integer, intent(in) :: m
+        integer, intent(in) :: n
+        integer, intent(in) :: mn(4)
+        integer, intent(in) :: lg(:,:)
+        integer, intent(in) :: lgrmin
+        integer(kind=int8), intent(in) :: area_mask(:,:)
+        integer, intent(in) :: quad_idx(:,:)
+        integer, intent(in), optional :: nod
+        integer, intent(inout), optional :: line(:,:)
+        integer, intent(inout), optional :: cross_pix_coords(:,:)
+        integer :: i0,i1,j0,j1,i2,i3,j2,j3
+        integer :: neighbour
+
+        !! U direction
+
+        call get_pix_corners(k, m, n, lgrmin, i0, i1, j0, j1, i2, i3, j2, j3)
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+        !!!!! U - direction !!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+        if (mn(3) < size(quad_idx, 1)) then
+            if(lg(mn(3)+1, mn(2)) == k .and. any(minval(area_mask(i1:i1+1,j0:j1), 1) > 0)) then
+                l_u = l_u + 1
+                if (present(line)) then
+                    neighbour = quad_idx(mn(3)+1, mn(2)) 
+                    ! We substract 1 from index to comply with C/python indexing.
+                    line(l_u,:) = (/ nod - 1, neighbour - 1/)
+                    cross_pix_coords(l_u,:) = (/ i1, j0 - 1, i1, j1 /) ! Python indexing so -1 at start slice
+                endif
+            else
+                if (lg(mn(3)+1, mn(2)) == k-1 .and. any(minval(area_mask(i1:i1+1,j0:j2), 1) > 0)) then
+                    l_u = l_u + 1
+                    if (present(line)) then
+                        neighbour = quad_idx(mn(3)+1, mn(2)) 
+                        ! We substract 1 from index to comply with C/python indexing.
+                        line(l_u,:) = (/ nod - 1, neighbour - 1 /)
+                        cross_pix_coords(l_u,:) = (/ i1, j0 - 1, i1, j2 /) ! Python indexing so -1 at start slice
+                    endif
+                endif
+                if (lg(mn(3)+1, mn(4)) == k-1 .and. any(minval(area_mask(i1:i1+1,j3:j1), 1) > 0)) then
+                    l_u = l_u + 1
+                    if (present(line)) then
+                        neighbour = quad_idx(mn(3)+1,mn(4))
+                        ! We substract 1 from index to comply with C/python indexing.
+                        line(l_u,:) = (/ nod - 1, neighbour - 1 /)
+                        cross_pix_coords(l_u,:) = (/ i1, j3 - 1, i1, j1 /) ! Python indexing so -1 at start slice
+                    endif
+                endif
+            endif
+        endif
+
+        if (mn(1) > 1) then
+            if(lg(mn(1)-1, mn(2)) == k-1 .and. any(minval(area_mask(i0-1:i0,j0:j2), 1) > 0)) then
+                l_u = l_u + 1
+                if (present(line)) then
+                    neighbour = quad_idx(mn(1)-1, mn(2))
+                    ! We substract 1 from index to comply with C/python indexing.
+                    line(l_u,:) = (/ neighbour - 1, nod - 1 /)
+                    cross_pix_coords(l_u,:) = (/ i0 - 1, j0 - 1, i0 - 1, j2 /)
+                endif
+            endif
+            if(lg(mn(1)-1, mn(4)) == k-1 .and. any(minval(area_mask(i0-1:i0,j3:j1), 1) > 0)) then
+                l_u = l_u + 1
+                if (present(line)) then
+                    neighbour = quad_idx(mn(1)-1, mn(4))
+                    ! We substract 1 from index to comply with C/python indexing.
+                    line(l_u,:) = (/ neighbour - 1, nod - 1 /)
+                    cross_pix_coords(l_u,:) = (/ i0 - 1, j3 - 1, i0 - 1, j1 /)
+                endif
+            endif
+        endif
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+        !!!!! U - direction !!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+        !!!!! V - direction !!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+        if (mn(4) < size(quad_idx, 2)) then
+            if (lg(mn(1), mn(4)+1) == k .and. any(minval(area_mask(i0:i1,j1:j1+1), 2) > 0)) then
+                l_v = l_v + 1
+                if (present(line)) then
+                    neighbour = quad_idx(mn(1), mn(4)+1)
+                    ! We substract 1 from index to comply with C/python indexing.
+                    line(l_v,:) = (/ nod - 1, neighbour - 1 /)
+                    cross_pix_coords(l_v,:) = (/ i0 - 1, j1, i1, j1 /) ! Python indexing so -1 at start slice
+                endif
+            else
+                if (lg(mn(1), mn(4)+1) == k-1 .and. any(minval(area_mask(i0:i2,j1:j1+1), 2) > 0)) then
+                    l_v = l_v + 1
+                    if (present(line)) then
+                        neighbour = quad_idx(mn(1), mn(4)+1)
+                        ! We substract 1 from index to comply with C/python indexing.
+                        line(l_v,:) = (/ nod - 1, neighbour - 1 /)
+                        cross_pix_coords(l_v,:) = (/ i0 - 1, j1, i2, j1 /) ! Python indexing so -1 at start slice
+                    endif
+                endif
+                if (lg(mn(3), mn(4)+1) == k-1 .and. any(minval(area_mask(i3:i1,j1:j1+1), 2) > 0)) then
+                    l_v = l_v + 1
+                    if (present(line)) then
+                        neighbour = quad_idx(mn(3), mn(4)+1)
+                        ! We substract 1 from index to comply with C/python indexing.
+                        line(l_v,:) = (/ nod - 1, neighbour - 1 /)
+                        cross_pix_coords(l_v,:) = (/ i3 - 1, j1, i1, j1 /) ! Python indexing so -1 at start slice
+                    endif
+                endif
+            endif
+        endif
+            
+        if (mn(2) > 1) then
+            if(lg(mn(1), mn(2)-1) == k-1 .and. any(minval(area_mask(i0:i2,max(1,j0-1):j0), 2) > 0)) then
+                l_v = l_v + 1
+                if (present(line)) then
+                    neighbour = quad_idx(mn(1), mn(2)-1)
+                    ! We substract 1 from index to comply with C/python indexing.
+                    line(l_v,:) = (/ neighbour - 1, nod - 1 /)
+                    cross_pix_coords(l_v,:) = (/ i0 - 1, j0 - 1, i2, j0 - 1/) ! Python indexing so -1 at start slice
+                endif
+            endif
+            if(lg(mn(3), mn(2)-1) == k-1 .and. any(minval(area_mask(i3:i1,max(1,j0-1):j0), 2) > 0)) then
+                l_v = l_v + 1
+                if (present(line)) then
+                    neighbour = quad_idx(mn(3), mn(2)-1)
+                    ! We substract 1 from index to comply with C/python indexing.
+                    line(l_v,:) = (/ neighbour - 1, nod - 1 /)
+                    cross_pix_coords(l_v,:) = (/ i3 - 1, j0 - 1, i1, j0 - 1 /) ! Python indexing so -1 at start slice
+                endif
+            endif
+        endif
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+        !!!!! V - direction !!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!
+
+    end subroutine set_2d_computational_lines
+
+
+end module m_cells
---
+++ libthreedigrid/quadtree.f90
@@ -0,0 +1,259 @@
+module m_quadtree
+
+    use parameters, only : NODATA
+    use iso_fortran_env, only : int8
+
+    implicit none
+
+    integer, parameter :: LINESTRING = 1
+    integer, parameter :: POLY = 3
+    integer, parameter :: UP = 1
+    integer, parameter :: DOWN = 0
+
+    contains
+
+    subroutine set_refinement(refine_id, refine_geom, refine_level,&
+        refine_type, bbox, mmax, nmax, dx, lg)
+        
+        use m_grid_utils, only : get_cell_geom, find_cell_intersects,&
+                                 geom_in_polygon,&
+                                 feature_in_bbox
+
+        double precision, intent(in) :: refine_geom(:, :) ! 2D array (x,y) of the refinement geometry
+        integer, intent(in) :: refine_id ! 2D ID of the refinement geometry  
+        integer, intent(in) :: refine_level ! The level of k to be set by the given geometry.
+        integer, intent(in) :: refine_type ! Type of geometry passed in the refine_geom variable (LINESTRING or POLYGON)
+        double precision, intent(in) :: bbox(4) ! Bounding box of total Quadtree
+        integer, intent(in) :: mmax(:) ! X Dimension of each refinement level
+        integer, intent(in) :: nmax(:) ! Y Dimension of each refinement level
+        double precision, intent(in) :: dx(:) ! Cell size of each refinement level
+        integer, intent(inout) :: lg(:, :) ! Array with all refinement levels.
+        integer :: status
+        integer :: m, n
+        integer :: mnmin(2), mnmax(2)
+        double precision :: cell_geom(5,2)
+        double precision :: refine_geom_bbox(4)
+        logical :: cross
+        
+        status = 0
+        if(.not.refine_type==LINESTRING.and..not.refine_type==POLY) then
+            write(*,*) '** WARNING: Refinement type not known. Skipping: ', refine_id, refine_type
+            status = 0
+            return
+        endif
+
+        refine_geom_bbox = (/ minval(refine_geom(:,1)), minval(refine_geom(:,2)),&
+                                maxval(refine_geom(:,1)), maxval(refine_geom(:,2)) /)
+        if (feature_in_bbox(refine_geom_bbox, bbox)) then
+            mnmin = convert_to_grid_crd(bbox(1:2), dx(1), refine_geom_bbox(1:2), mmax, nmax, DOWN)
+            mnmax = convert_to_grid_crd(bbox(1:2), dx(1), refine_geom_bbox(3:4), mmax, nmax, UP)
+        else
+            write(*,*) '** INFO: Refinement outside area_mask. ID and type: ', refine_id, refine_type
+            status = 0
+            return
+        endif
+
+        write(*,*) '** INFO: Start applying refinement with refinement level and type: ', refine_id, refine_level, refine_type
+        cross=.FALSE.
+        do n=mnmin(2), mnmax(2)
+            do m=mnmin(1), mnmax(1)
+                cell_geom = get_cell_geom(bbox(1), bbox(2), m, n, dx(1))
+                if (minval(cell_geom(:,1))>=minval(refine_geom(:,1)) - dx(1).and.&
+                    maxval(cell_geom(:,1))<=maxval(refine_geom(:,1)) + dx(1).and.&
+                    minval(cell_geom(:,2))>=minval(refine_geom(:,2)) - dx(1).and.&
+                    maxval(cell_geom(:,2))<=maxval(refine_geom(:,2)) + dx(1)) then
+                    if (refine_type==LINESTRING) then
+                        cross = find_cell_intersects(refine_geom, cell_geom)  !!TODO: Check linestrings that fall within smallest cell of quadtree.
+                    elseif(refine_type==POLY) then
+                        cross = geom_in_polygon(refine_geom, cell_geom)
+                    endif
+
+                    if (cross) then
+                        lg(m,n) = min(lg(m,n), refine_level)
+                        status = 1
+                    endif
+                else
+                    cycle
+                endif
+            enddo
+        enddo
+
+        if (status == 0) then
+            write(*,*) '** WARNING: Unsuccessfully applied refinement geometry with id: ', refine_id, ' and type: ', refine_type
+        endif
+
+    end subroutine set_refinement
+
+    subroutine make_quadtree(kmax, mmax, nmax, lgrmin, area_mask, quad_idx, lg, n_cells, n_lines)
+    !!! Entry point for creating quadtree by setting lg array and then finding number of active cells and lines.
+        use iso_fortran_env, only : int8
+
+        integer, intent(in) :: kmax ! Maximum refinement levels
+        integer, intent(in) :: mmax(:) ! X Dimension of each refinement level
+        integer, intent(in) :: nmax(:) ! Y Dimension of each refinement level
+        integer, intent(in) :: lgrmin ! Number of pixels in cell of smallest refinement level
+        integer*2, intent(in) :: area_mask(:, :) ! Array with active pixels of model.
+        integer, intent(inout) :: lg(:, :) ! Array with all refinement levels.
+        integer, intent(inout) :: quad_idx(:, :) ! Array with idx of cell at lg refinement locations
+        integer, intent(inout) :: n_cells ! counter for active cells
+        integer, intent(inout) :: n_lines(2) ! counter for active u lines
+        integer :: m, n
+
+        write(*,*) '** INFO: Start making quadtree.'
+        do m=1, mmax(kmax)
+            do n=1, nmax(kmax)
+                call divide(kmax, m, n, lg)
+            enddo
+        enddo
+        call balance_quadtree(kmax, mmax, nmax, lg)
+        call find_active_2d_comp_cells(kmax, mmax, nmax, lgrmin, lg, area_mask, quad_idx, n_cells, n_lines(1), n_lines(2))
+        write(*,*) '** INFO: Done making quadtree.'
+
+    end subroutine make_quadtree
+
+    recursive subroutine divide(k, m, n, lg) !ip, jp, 
+    !!! Recursive subroutine to set correct refinement levels on lg refinement array.
+        use m_grid_utils, only : get_lg_corners
+
+        integer, intent(in) :: k
+        integer, intent(in) :: m
+        integer, intent(in) :: n
+        integer, intent(inout) :: lg(:,:)
+        integer :: k1
+        integer :: mn(4)
+        integer :: m_n, n_n
+
+        mn = get_lg_corners(k, m, n)
+        if(any(lg(mn(1):mn(3),mn(2):mn(4)) < k).and.k>1) then
+            k1 = k-1
+            m_n=2*m-1
+            n_n=2*n-1
+            lg(mn(1):mn(3),mn(2):mn(4)) = min(lg(mn(1):mn(3),mn(2):mn(4)), k1)
+            call divide(k1, m_n, n_n, lg)
+            call divide(k1, m_n+1, n_n, lg)
+            call divide(k1, m_n, n_n+1, lg)
+            call divide(k1, m_n+1, n_n+1, lg)
+        endif
+
+    end subroutine divide
+
+    subroutine balance_quadtree(kmax, mmax, nmax, lg)
+    !!! Balancing out refinement levels, so that neighbouring cells are never more than 1 refinement level apart.
+        integer, intent(in) :: kmax
+        integer, intent(in) :: mmax(:)
+        integer, intent(in) :: nmax(:)
+        integer, intent(inout) :: lg(:,:)
+        integer :: k
+        integer :: m, n
+
+        do k=1,kmax-1
+            do n=1, nmax(1)
+                do m=1,mmax(1)-1
+                    if(lg(m,n) == k .and. lg(m+1,n)>k+1) then
+                        lg(m+1,n)=k+1
+                    endif
+                    if(lg(m,n) > k+1 .and. lg(m+1,n)==k) then
+                        lg(m,n)=k+1
+                    endif
+                enddo
+            enddo
+            do n=1,nmax(1)-1
+                do m=1, mmax(1)
+                    if (lg(m,n)==k.and.lg(m,n+1)>k+1) then
+                        lg(m,n+1)=k+1
+                    endif
+                    if (lg(m,n)>k+1.and.lg(m,n+1)==k) then
+                        lg(m,n)=k+1
+                    endif
+                enddo
+            enddo
+
+            
+            do n=1, nmax(kmax)
+                do m=1, mmax(kmax)
+                    call divide(kmax, m, n, lg)
+                enddo
+            enddo
+        enddo
+
+    end subroutine balance_quadtree
+
+    subroutine find_active_2d_comp_cells(kmax, mmax, nmax, lgrmin, lg, area_mask, quad_idx, n_cells, n_line_u, n_line_v)
+    !!! Counting active cells and lines based on area_mask of active pixels.
+        use m_grid_utils, only : get_lg_corners, get_pix_corners, crop_pix_coords_to_raster, pad_area_mask
+        use m_cells, only : set_2d_computational_lines
+        use iso_fortran_env, only : int8
+
+        integer, intent(in) :: kmax
+        integer, intent(in) :: mmax(:)
+        integer, intent(in) :: nmax(:)
+        integer, intent(in) :: lgrmin
+        integer, intent(inout) :: lg(:,:)
+        integer(kind=int8), intent(in) :: area_mask(:,:)
+        integer, intent(inout) :: quad_idx(:,:)
+        integer, intent(inout) :: n_line_u
+        integer, intent(inout) :: n_line_v
+        integer(kind=int8), allocatable:: area_mask_padded(:, :)
+        integer :: k
+        integer :: m,n
+        integer :: mn(4)
+        integer :: i0, i1, j0, j1
+        integer :: n_cells
+        
+        
+        n_cells = 0
+        n_line_u = 0
+        n_line_v = 0
+        quad_idx = 0
+        call get_pix_corners(kmax, mmax(kmax), nmax(kmax), lgrmin, i0, i1, j0, j1)
+        area_mask_padded = pad_area_mask(area_mask, i0, i1, j0, j1) 
+        do k=kmax,1,-1
+            do m=1,mmax(k)
+                do n=1,nmax(k)
+                    call get_pix_corners(k, m, n, lgrmin, i0, i1, j0, j1)
+                    mn = get_lg_corners(k, m, n)
+                    i1 = min(i1, size(area_mask, 1))
+                    j1 = min(j1, size(area_mask, 2))
+                    if (all(lg(mn(1):mn(3),mn(2):mn(4)) == k)) then !! TODO: CHECK OF MODEL AREA CHECK IS NECESSARY???
+                        if (all(area_mask_padded(i0:i1, j0:j1) == 0)) then
+                            lg(mn(1):mn(3),mn(2):mn(4)) = -99
+                        else
+                            n_cells = n_cells + 1
+                            lg(mn(1):mn(3),mn(2):mn(4)) = k   !! DO WE OVERWRITE AND FAVOR LARGER CELLS
+                            quad_idx(mn(1):mn(3),mn(2):mn(4)) = n_cells
+                            call set_2d_computational_lines(n_line_u, n_line_v, k, m, n, mn, lg, lgrmin, area_mask_padded, quad_idx)
+                        endif
+                    endif
+                enddo
+            enddo
+        enddo
+        deallocate(area_mask_padded)
+        write(*,*) '** INFO: No. active 2D computational cells: ', n_cells
+        write(*,*) '** INFO: Number of 2D Surface flow lines is: ', n_line_u, n_line_v
+
+    end subroutine find_active_2d_comp_cells
+    
+    function convert_to_grid_crd(origin, dx, xy, mmax, nmax, round) result (mn)
+    !!! Create pixel indexes (or grid coordinates) for lg array.
+        double precision, intent(in) :: xy(2)
+        double precision, intent(in) :: origin(2)
+        double precision, intent(in) :: dx
+        integer, intent(in) :: mmax(:)
+        integer, intent(in) :: nmax(:)
+        integer, intent(in) :: round
+        integer :: mn(2)
+
+        if (round == DOWN) then
+            mn(1) = max(1, int(floor((xy(1) - origin(1) - dx) / dx)))
+            mn(2) = max(1, int(floor((xy(2) - origin(2) - dx) / dx)))
+        elseif (round == UP) then
+            mn(1) = min(mmax(1), int(ceiling((xy(1) - origin(1)) / dx)))
+            mn(2) = min(nmax(1), int(ceiling((xy(2) - origin(2)) / dx)))
+        else
+            write(*,*) '** ERROR: Rounding option not known: ', round
+        endif
+
+    end function convert_to_grid_crd
+
+
+end module m_quadtree
