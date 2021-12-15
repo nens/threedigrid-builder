323e86d73448ef753db262d55d55e198e7fcc32c
Martijn Siemerink <martijn.siemerink@nelen-schuurmans.nl>
2021-12-15 10:03:29 +0100

    Cleanup.

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ ext_modules = [
             "./libthreedigrid/cells.f90",
             "./libthreedigrid/quadtree.f90",
         ],
-        f2py_options= f2py_options + ["f2cmap", "./libthreedigrid/.f2py_f2cmap"],
+        f2py_options= f2py_options + ["--f2cmap"],
         extra_f90_compile_args=comp_flags,
         define_macros=macro + [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     )
