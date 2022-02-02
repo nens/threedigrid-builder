01466443cda72d246444ed4d7f9ee7cb63a1cb32
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-02-02 15:47:34 +0100

    Last unittest

--- threedigrid_builder/tests/test_raster_interfaces.py
+++ threedigrid_builder/tests/test_raster_interfaces.py
@@ -11,11 +11,9 @@ def test_does_not_exist(tmp_path):
             pass
 
 
-def test_epsg_code(dem_path):
+def test_crs_wkt(dem_path, crs_wkt_28992_legacy):
     with GDALInterface(dem_path) as dem:
-        assert dem.crs == CRS(
-            'PROJCS["Amersfoort / RD New",GEOGCS["Amersfoort",DATUM["Amersfoort",SPHEROID["Bessel 1841",6377397.155,299.1528128,AUTHORITY["EPSG","7004"]],TOWGS84[565.2369,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812],AUTHORITY["EPSG","6289"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4289"]],PROJECTION["Oblique_Stereographic"],PARAMETER["latitude_of_origin",52.1561605555556],PARAMETER["central_meridian",5.38763888888889],PARAMETER["scale_factor",0.9999079],PARAMETER["false_easting",155000],PARAMETER["false_northing",463000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","28992"]]'
-        )
+        assert dem.crs_wkt == crs_wkt_28992_legacy
 
 
 def test_pixel_size(dem_path):
