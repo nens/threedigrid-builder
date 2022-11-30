671dd667ab2bbd8f1a2b67c0bd7766450bb0c9de
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-11-30 17:06:10 +0100

    WIP

--- threedigrid_builder/grid/exchange_lines.py
+++ threedigrid_builder/grid/exchange_lines.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import numpy as np
 import pygeos
 
-from threedigrid_builder.base import Array, search
+from threedigrid_builder.base import Array, Nodes, search
 from threedigrid_builder.constants import CalculationType, ContentType
 from threedigrid_builder.exceptions import SchematisationError
 
@@ -46,13 +46,12 @@ class ExchangeLines(Array[ExchangeLine]):
         result[is_channel] = ids
         return result
 
-    def get_line_mappings(self, nodes):
+    def get_line_mappings(self, nodes: Nodes):
         """Return a node index and an exchange line id per 1D2D line.
 
-        Returns 3 arrays of shape (n_lines, ):
+        Returns 2 arrays of shape (n_lines, ):
         - an int array with a node index
-        - an int array with a exchange line index (-9999 if there is none)
-        - a bool array with True where a line is double connected
+        - an int array with a exchange line id (-9999 if there is none)
         """
         primary, secondary = self.split_primary_secondary()
         # Initialize the arrays
@@ -76,3 +75,102 @@ class ExchangeLines(Array[ExchangeLine]):
         # Sort them by node index
         sorter = np.argsort(node_idx)
         return node_idx[sorter], exc_id[sorter]
+
+    def get_2d_side_points(self, nodes: Nodes):
+        """Return a 2D side Point for each line
+
+        Returns 3 arrays of shape (n_lines, ):
+        - an int array with a node index
+        - an int array with a exchange line id (-9999 if there is none)
+        - a Geometry array with points
+        """
+        # Create an array of node indices & corresponding exchange line indices
+        node_idx, exc_id = self.get_line_mappings(nodes)
+        has_exc = exc_id != -9999
+
+        # Collect the 1D sides of the 1D2D line
+        geometries = pygeos.points(nodes.coordinates[node_idx])
+
+        # Change these into the 2D sides through the exchange line
+        # if there is no exchange line, 2D side equals 1D side.
+        exchange_line_geoms = self.the_geom[self.id_to_index(exc_id[has_exc])]
+        pygeos.prepare(exchange_line_geoms)
+        geometries[has_exc] = pygeos.get_point(
+            pygeos.shortest_line(exchange_line_geoms, geometries[has_exc]), 0
+        )
+
+        return node_idx, exc_id, geometries
+
+    def get_lines_node_idx(self, nodes: Nodes, cell_tree: pygeos.STRtree):
+        """Return a 1D and 2D node index per line
+
+        Returns 3 arrays of shape (n_lines, ):
+        - an int array with a 2D node (cell) index
+        - an int array with a 1D node index
+        - an int array with a exchange line id (-9999 if there is none)
+        """
+        node_idx, exc_id, side_2d = self.get_2d_side_points(nodes)
+        # The query_bulk returns 2 1D arrays: one with indices into the supplied node
+        # geometries and one with indices into the tree of cells.
+        idx = cell_tree.query_bulk(side_2d)
+        # Address edge cases of multiple 1D-2D lines per node: just take the one
+        _, first_unique_index = np.unique(idx[0], return_index=True)
+        idx = idx[:, first_unique_index]
+        n_lines = idx.shape[1]
+        # Error if there is a node without a 1D-2D line
+        if n_lines != node_idx.shape[0]:
+            # The code in this if clause is only for pretty error formatting.
+            out_of_bounds = np.delete(node_idx, idx[0])
+            out_of_bounds_exc = np.delete(exc_id, idx[0])
+            object_pk_list = _get_pk_content_type(
+                nodes, out_of_bounds[out_of_bounds_exc == -9999]
+            )
+            msg = ""
+            if len(object_pk_list) > 0:
+                msg += (
+                    f"The following object(s) have a connected calculation type but are "
+                    f"(partially) outside of the 2D calculation cells: "
+                    f"{', '.join(object_pk_list)}. "
+                )
+            if (out_of_bounds_exc != -9999).any():
+                mask = out_of_bounds_exc != -9999
+                exc_ids_list = out_of_bounds_exc[mask].tolist()
+                msg += (
+                    f"The following exchange line(s) are outside of a 2D calculation "
+                    f"cell: {str(exc_ids_list)}."
+                )
+            raise SchematisationError(msg)
+
+        return idx[1], node_idx[idx[0]], exc_id
+
+
+HAS_1D2D_CONTENT_TYPES = (
+    ContentType.TYPE_V2_CONNECTION_NODES,
+    ContentType.TYPE_V2_CHANNEL,
+    ContentType.TYPE_V2_PIPE,
+    ContentType.TYPE_V2_CULVERT,
+)
+
+
+def _get_pk_content_type(nodes, out_of_bounds):
+    """Determine the object pk and object type of calculation points for pretty
+    printing.
+
+    Args:
+      nodes (Nodes):
+      out_of_bounds (ndarray): Array with node idx of nodes that are out of bounds.
+
+    Returns:
+      List of content types (their pretty name) and pks of objects with nodes
+      out of bounds.
+    """
+    if len(out_of_bounds) == 0:
+        return []
+    types = nodes.content_type[out_of_bounds]
+    pks = nodes.content_pk[out_of_bounds]
+    pretty_names = ("connection nodes", "channels", "pipes", "culverts")
+    return [
+        f"{pretty_name} {sorted(set(pks[types == content_type]))}"
+        for content_type, pretty_name in zip(HAS_1D2D_CONTENT_TYPES, pretty_names)
+        if np.any(types == content_type)
+    ]
--- threedigrid_builder/tests/test_exchange_lines.py
+++ threedigrid_builder/tests/test_exchange_lines.py
@@ -1,10 +1,15 @@
+from unittest import mock
+
+import numpy as np
+import pygeos
 import pytest
 from numpy.testing import assert_array_equal
+from pygeos.testing import assert_geometries_equal
 
-from threedigrid_builder.base import Nodes
-from threedigrid_builder.constants import CalculationType, ContentType
+from threedigrid_builder.base import Lines, Nodes
+from threedigrid_builder.constants import CalculationType, ContentType, NodeType
 from threedigrid_builder.exceptions import SchematisationError
-from threedigrid_builder.grid import ExchangeLines
+from threedigrid_builder.grid import ExchangeLines, Grid
 
 
 @pytest.mark.parametrize(
@@ -76,3 +81,90 @@ def test_get_line_mappings_too_many_exchange_lines():
         SchematisationError, match=r"Channels \[1\] have too many exchange lines"
     ):
         exchange_lines.get_line_mappings(Nodes(id=[]))
+
+
+def test_get_2d_side_points():
+    nodes = Nodes(
+        id=[0, 1, 2],
+        coordinates=[(2, 5), (4, 5), (6, 5)],
+    )
+    exchange_lines = ExchangeLines(
+        id=[1, 2],
+        the_geom=pygeos.linestrings([[[0, 0], [10, 0]], [[0, 10], [10, 10]]]),
+    )
+    # nodes have respectively: 2 exchange lines, 1 exchange line, no exchange lines
+    node_idx = np.array([0, 0, 1, 2])
+    exc_ids = np.array([1, 2, 1, -9999])
+
+    expected_geometries = pygeos.points([[2, 0], [2, 10], [4, 0], [6, 5]])
+
+    with mock.patch.object(
+        exchange_lines, "get_line_mappings", return_value=(node_idx, exc_ids)
+    ):
+        actual = exchange_lines.get_2d_side_points(nodes)
+
+        assert actual[0] is node_idx
+        assert actual[1] is exc_ids
+        actual_geometries = actual[2]
+
+    assert_geometries_equal(actual_geometries, expected_geometries)
+
+
+@pytest.fixture
+def grid2d():
+    return Grid(
+        nodes=Nodes(
+            id=[0, 1],
+            node_type=NodeType.NODE_2D_OPEN_WATER,
+            bounds=[(0, 0, 1, 1), (1, 0, 3, 2)],
+        ),
+        lines=Lines(id=[0]),
+    )
+
+
+@pytest.mark.parametrize(
+    "side_2d_coordinates,expected_lines",
+    [
+        ([(0.5, 0.5)], [(0, 2)]),  # first cell, center
+        ([(0, 0.5)], [(0, 2)]),  # first cell, left edge
+        ([(0.5, 1)], [(0, 2)]),  # first cell, top edge
+        ([(0.5, 0)], [(0, 2)]),  # first cell, bottom edge
+        ([(0, 1)], [(0, 2)]),  # first cell, topleft corner
+        ([(0, 0)], [(0, 2)]),  # first cell, bottomleft corner
+        ([(2, 1)], [(1, 2)]),  # second cell, center
+        ([(3, 1)], [(1, 2)]),  # second cell, right edge
+        ([(2, 2)], [(1, 2)]),  # second cell, top edge
+        ([(2, 0)], [(1, 2)]),  # second cell, bottom edge
+        ([(3, 2)], [(1, 2)]),  # second cell, topright corner
+        ([(3, 0)], [(1, 2)]),  # second cell, bottomright corner
+        ([(1, 1)], [(0, 2)]),  # edge between: top corner
+        ([(1, 0)], [(0, 2)]),  # edge between: bottom corner
+        ([(1, 0.5)], [(0, 2)]),  # edge between: middle
+        ([(0.5, 0.5), (0.5, 0.9)], [(0, 2), (0, 3)]),  # two cells, same
+        ([(0.5, 0.5), (2, 1)], [(0, 2), (1, 3)]),  # two cells, different
+    ],
+)
+def test_get_lines_node_idx(side_2d_coordinates, expected_lines, grid2d):
+    grid2d.nodes += Nodes(
+        id=[7, 8][: len(side_2d_coordinates)],
+        coordinates=side_2d_coordinates,
+    )
+    exchange_lines = ExchangeLines(id=[])
+    node_idx = np.array([2, 3][: len(side_2d_coordinates)])
+    exc_ids = np.array([-9999, -9999][: len(side_2d_coordinates)])
+    side_2d_geometries = pygeos.points(side_2d_coordinates)
+
+    with mock.patch.object(
+        exchange_lines,
+        "get_2d_side_points",
+        return_value=(node_idx, exc_ids, side_2d_geometries),
+    ):
+        idx_1d, idx_2d, exc_ids_ = exchange_lines.get_lines_node_idx(
+            grid2d.nodes, grid2d.cell_tree
+        )
+
+        assert exc_ids is exc_ids_
+
+    expected_idx_1d, expected_idx_2d = np.array(expected_lines).T
+    assert_array_equal(idx_1d, expected_idx_1d)
+    assert_array_equal(idx_2d, expected_idx_2d)
