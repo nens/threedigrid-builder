686ce2eeaa6da24b90f9f0e97bb643ca5d4ecbf6
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-02-23 11:27:23 +0100

    rewrite numpy test asserts

--- threedigrid_builder/tests/test_lines_1d2d.py
+++ threedigrid_builder/tests/test_lines_1d2d.py
@@ -185,10 +185,9 @@ def test_remove_line_unassigned_nodes():
         id=[1, 2], line=[(-9999, 1), (1, 2)], line_coords=[(10, 0, 0, 0), (5, 2, 3, 8)]
     )
     lines = lines.remove_unassigned(nodes)
-    assert lines.id == [2]
-    assert (lines.line == [(1, 2)]).all()
-    assert (lines.line_coords == [(5, 2, 3, 8)]).all()
-    pass
+    np.testing.assert_equal(lines.id, [2])
+    np.testing.assert_equal(lines.line, [(1, 2)])
+    np.testing.assert_equal(lines.line_coords, [(5, 2, 3, 8)])
 
 
 def test_assign_kcu():
