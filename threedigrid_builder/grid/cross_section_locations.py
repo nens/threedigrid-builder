4a259bb99a23fc8be0b93cbfdd244d9e842ab113
Golnesa <golnesa.karimizindashti@nelen-schuurmans.nl>
2024-02-01 14:23:03 +0100

    update no 'single' vegetation

--- threedigrid_builder/grid/cross_section_locations.py
+++ threedigrid_builder/grid/cross_section_locations.py
@@ -70,18 +70,22 @@ class CrossSectionLocations(Array[CrossSectionLocation]):
         lines.frict_type2 = self.friction_type[idx2]
         lines.frict_value1 = self.friction_value[idx1]
         lines.frict_value2 = self.friction_value[idx2]
+
         lines.veg_coef1 = (
             self.vegetation_stem_density[idx1]
             * self.vegetation_stem_diameter[idx1]
             * self.vegetation_height[idx1]
             * self.vegetation_drag_coefficient[idx1]
         )
+        lines.veg_coef1[np.isnan(lines.veg_coef1)] = 0.0
+
         lines.veg_coef2 = (
             self.vegetation_stem_density[idx2]
             * self.vegetation_stem_diameter[idx2]
             * self.vegetation_height[idx2]
             * self.vegetation_drag_coefficient[idx2]
         )
+        lines.veg_coef2[np.isnan(lines.veg_coef2)] = 0.0
 
         # Compute invert levels and start and end
         lines.invert_level_start_point = compute_bottom_level(
