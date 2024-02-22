acb1a1ecc002c584dc74e36108dde0e02c3029f5
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-02-22 13:38:16 +0100

    Clarify skip reason

--- threedigrid_builder/tests/test_raster_interfaces.py
+++ threedigrid_builder/tests/test_raster_interfaces.py
@@ -13,7 +13,7 @@ def test_does_not_exist(tmp_path):
 
 @pytest.mark.skipif(
     int(gdal.VersionInfo()[:3]) >= 304,
-    reason="this test is known to fail with GDAL 3.4",
+    reason="this test is known to fail with GDAL versions below 3.4",
 )
 def test_crs_legacy(dem_path, crs_wkt_28992_legacy):
     with GDALInterface(dem_path) as dem:
@@ -22,7 +22,7 @@ def test_crs_legacy(dem_path, crs_wkt_28992_legacy):
 
 @pytest.mark.skipif(
     int(gdal.VersionInfo()[:3]) < 304,
-    reason="this test is known to fail with GDAL 3.4",
+    reason="this test is known to fail with GDAL 3.4 and newer",
 )
 def test_crs(dem_path, crs_wkt_28992):
     with GDALInterface(dem_path) as dem:
