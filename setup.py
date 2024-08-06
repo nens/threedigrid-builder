30aaffdf431412fe155a07e8e6868da87b740ba8
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-08-06 16:14:58 +0200

    Use schema 224 from git repo

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ def get_version():
 install_requires = [
     "numpy>=1.15,<3.0",
     "threedi-schema==0.223.*",
+    "threedi-schema@git+https://github.com/nens/threedi-schema@margriet_70_schema_300_structure_control",
     "shapely>=2",
     "pyproj>=3",
     "condenser[geo]>=0.1.1",
