9e5957ee4ae688d6b050d6059aa94656b2302dd4
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-08-12 10:46:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -53,8 +53,7 @@ def get_version():
 
 install_requires = [
     "numpy>=1.15,<3.0",
-    "threedi-schema==0.224.0.dev1",
-    # "threedi-schema@git+https://github.com/nens/threedi-schema@margriet_70_schema_300_structure_control",
+    "threedi-schema==0.224.0.dev2",
     "shapely>=2",
     "pyproj>=3",
     "condenser[geo]>=0.1.1",
