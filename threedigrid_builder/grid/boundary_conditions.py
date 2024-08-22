e22a87393c90d87ef5fa6a4a38532adac8e90336
elisalle <eli.salle@nelen-schuurmans.nl>
2024-08-22 14:49:23 +0200

    rename boundarytype function

--- threedigrid_builder/grid/boundary_conditions.py
+++ threedigrid_builder/grid/boundary_conditions.py
@@ -187,7 +187,7 @@ class BoundaryConditions2D(Array[BoundaryCondition2D]):
         return node_idx[before if is_before else after], is_before
 
     def adapt_node_idx_groundwater(self, idx: int, nodes: Nodes, node_idx):
-        is_groundwater = self.boundary_type_is_groundwater(self.boundary_type[idx])
+        is_groundwater = self.type_is_groundwater(self.type[idx])
         if is_groundwater:
             n_groundwater_cells = nodes.n_groundwater_cells
             if n_groundwater_cells == 0:
@@ -266,8 +266,8 @@ class BoundaryConditions2D(Array[BoundaryCondition2D]):
         }
 
     @staticmethod
-    def boundary_type_is_groundwater(boundary_type: BoundaryType) -> bool:
-        return boundary_type in {
+    def type_is_groundwater(type: BoundaryType) -> bool:
+        return type in {
             BoundaryType.GROUNDWATERLEVEL,
             BoundaryType.GROUNDWATERDISCHARGE,
         }
