411faa658cc7309bc09dbb8326d9c6a0e90239e0
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-09-03 13:44:55 +0200

    Install schema from github zip

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def get_version():
 
 install_requires = [
     "numpy>=1.15,<3.0",
-    "threedi-schema==0.224.*",
+    "threedi-schema @ https://github.com/nens/threedi-schema/archive/margriet_73_schema_300_2d_1d2d.zip",
     "shapely>=2",
     "pyproj>=3",
     "condenser[geo]>=0.1.1",
