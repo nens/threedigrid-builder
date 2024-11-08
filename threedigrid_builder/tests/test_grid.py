691db47fafb308e46eef596d56c59747bad913d1
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2024-11-08 11:59:23 +0100

    partial cut test

--- threedigrid_builder/tests/test_grid.py
+++ threedigrid_builder/tests/test_grid.py
@@ -381,6 +381,13 @@ def test_split_single_cut(cell_polygon):
     )  # upper-left triangle
 
 
+def test_split_partial_cut(cell_polygon):
+    cutlines = [shapely.LineString([(0, 0), (5, 5)])]
+    fragments = Grid.split(cell_polygon, cutlines)
+    assert len(fragments) == 1
+    assert fragments[0].equals_exact(cell_polygon, tolerance=0.0)
+
+
 def test_split_double_cut(cell_polygon):
     cutlines = [
         shapely.LineString([(0, 0), (10, 10)]),
