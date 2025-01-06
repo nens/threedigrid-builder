d4c793d0939329a547f9600c16138fb88832d95b
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2025-01-06 15:09:35 +0100

    Fix schema dependency

--- setup.py
+++ setup.py
@@ -53,7 +53,8 @@ def get_version():
 
 install_requires = [
     "numpy>=1.15,<3.0",
-    "threedi-schema==0.228.*",
+    "threedi-schema @ git+https://github.com/nens/threedi-schema.git@margriet_schema_300_leftovers",
+    # "threedi-schema==0.228.*",
     "shapely>=2",
     "pyproj>=3",
     "condenser[geo]>=0.1.1",
