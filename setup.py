e0633b02169ba4f33301249b171136550ece5f16
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2025-01-08 07:41:48 +0100

    Set correct schema version

--- setup.py
+++ setup.py
@@ -53,8 +53,7 @@ def get_version():
 
 install_requires = [
     "numpy>=1.15,<3.0",
-    "threedi-schema @ git+https://github.com/nens/threedi-schema.git@margriet_schema_300_leftovers",
-    # "threedi-schema==0.228.*",
+    "threedi-schema==0.229.*",
     "shapely>=2",
     "pyproj>=3",
     "condenser[geo]>=0.1.1",
