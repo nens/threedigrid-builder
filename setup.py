4a3e2901422c0b88826bcb35685266ed21643cb0
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2025-01-20 09:19:41 +0100

    Set correct threedi-schema version

--- setup.py
+++ setup.py
@@ -53,7 +53,8 @@ def get_version():
 
 install_requires = [
     "numpy>=1.15,<3.0",
-    "threedi-schema==0.230.*",
+    #"threedi-schema==0.230.*",
+    "threedi-schema @ git+ssh://git@github.com/nens/threedi-schema@margriet_119_orm_class_names",
     "shapely>=2",
     "pyproj>=3",
     "condenser[geo]>=0.1.1",
