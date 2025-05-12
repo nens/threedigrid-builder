996275d0c17c6c24831dbd4018a6ea73059cdc19
Martijn Siemerink <martijn.siemerink@nelen-schuurmans.nl>
2025-05-12 14:06:03 +0200

    print statements.

--- libthreedigrid/quadtree.f90
+++ libthreedigrid/quadtree.f90
@@ -1,6 +1,5 @@
 module m_quadtree
 
-    use iso_fortran_env, only: output_unit, error_unit
     use parameters, only : NODATA
 
     implicit none
@@ -26,25 +25,23 @@ module m_quadtree
         integer :: k
         integer :: m, n
 
-        open(output_unit, file=trim("D:/tmp/stdout.log"))
-        open(error_unit,file=trim("D:/tmp/stderr.log"))
-        write(error_unit, *) "HELLO!"
+        open(77, file=trim("D:/tmp/stdout.log"))
+
         write(*,*) '** INFO: Start making quadtree.'
         do m=1, mmax(kmax)
             do n=1, nmax(kmax)
                 call divide(kmax, m, n, lg)
             enddo
         enddo
-        write(output_unit,*) "A"
+        write(77,*) "A"
         call balance_quadtree(kmax, mmax, nmax, lg)
-        write(output_unit,*) "B"
+        write(77,*) "B"
         call find_active_2d_comp_cells(&
             kmax, mmax, nmax, lgrmin, use_2d_flow > 0, lg, area_mask, quad_idx, n_cells, n_line_u, n_line_v&
         )
-        write(output_unit,*) "C"
+        write(77,*) "C"
         write(*,*) '** INFO: Done making quadtree.'
-        close(output_unit)
-        close(error_unit)
+        close(77)
 
     end subroutine make_quadtree
 
@@ -141,36 +138,36 @@ module m_quadtree
         integer :: i0, i1, j0, j1, i2, i3, j2, j3
         integer :: n_cells
         
-        write(output_unit,*) "AA"
+        write(77,*) "AA"
         n_cells = 0
         n_line_u = 0
         n_line_v = 0
         quad_idx = 0
-        write(output_unit,*) "BB"
+        write(77,*) "BB"
         call get_pix_corners(kmax, mmax(kmax), nmax(kmax), lgrmin, i0, i1, j0, j1)
-        write(output_unit,*) "CC"
+        write(77,*) "CC"
         area_mask_padded = pad_area_mask(area_mask, i0, i1, j0, j1) 
-        write(output_unit,*) "DD"
+        write(77,*) "DD"
         do k=kmax,1,-1
             do m=1,mmax(k)
                 do n=1,nmax(k)
-                    write(output_unit,*) "EE"
-                    flush(output_unit)
+                    write(77,*) "EE"
+                    flush(77)
                     call get_pix_corners(k, m, n, lgrmin, i0, i1, j0, j1)
                     mn = get_lg_corners(k, m, n)
-                    write(output_unit,*) "FF", i1, size(area_mask, 1)
-                    flush(output_unit)
+                    write(77,*) "FF", i1, size(area_mask, 1)
+                    flush(77)
                     i1 = min(i1, size(area_mask, 1))
-                    write(output_unit,*) "GG", i1
-                    flush(output_unit)
-                    write(output_unit,*) "HH", j1, size(area_mask, 2)
-                    flush(output_unit)
+                    write(77,*) "GG", i1
+                    flush(77)
+                    write(77,*) "HH", j1, size(area_mask, 2)
+                    flush(77)
                     j1 = min(j1, size(area_mask, 2))
-                    write(output_unit,*) "II", j1, size(area_mask, 2)
-                    flush(output_unit)
+                    write(77,*) "II", j1, size(area_mask, 2)
+                    flush(77)
                     if (all(lg(mn(1):mn(3),mn(2):mn(4)) == k)) then !! TODO: CHECK OF MODEL AREA CHECK IS NECESSARY???
-                        write(output_unit,*) "JJ", all(lg(mn(1):mn(3),mn(2):mn(4)) == k), k
-                        flush(output_unit)
+                        write(77,*) "JJ", all(lg(mn(1):mn(3),mn(2):mn(4)) == k), k
+                        flush(77)
                         if (all(area_mask_padded(i0:i1, j0:j1) == 0)) then
                             lg(mn(1):mn(3),mn(2):mn(4)) = -99
                         else
@@ -184,8 +181,8 @@ module m_quadtree
                             endif
                         endif
                     endif
-                    write(output_unit,*) "KK", all(lg(mn(1):mn(3),mn(2):mn(4)) == k), k
-                    flush(output_unit)
+                    write(77,*) "KK", all(lg(mn(1):mn(3),mn(2):mn(4)) == k), k
+                    flush(77)
                 enddo
             enddo
         enddo
